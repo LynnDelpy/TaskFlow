24bce600ed4a8d427313b04e68bc4586d5aef55d
Lynnnn15 <delpymalek1@gmail.com>
2024-06-05 13:01:38 +0200

    basics now working

--- src/ToDo.vue
+++ src/ToDo.vue
@@ -1,16 +1,25 @@
 <template>
   <main class="grid">
     <div class="applicationContainer">
-      <div class="textContainer1">
-        <p id="datetime">{{ datetimeArray[0] }}</p> <br><br>
-        <p id="datetime2">{{ datetimeArray[1] }}</p>
-      </div>
-      <div class="textContainer2">
-
+      <div class="textContainers">
+        <div class="textContainer1">
+          <p id="datetime">{{ datetimeArray[0] }}</p>
+          <p id="datetime2">{{ dateName }}</p>
+        </div>
+        <div class="textContainer2">
+          <p id="taskText">Task</p>
+          <p id="flowText">Flow</p>
+        </div>
       </div>
       <div class="inputBox">
-        <input v-model="newTodo" placeholder="Enter a To-Do item..." type="text"/>
-        <button @click="addTodo">Add To-Do</button>
+
+        <input v-model="newTodo" placeholder="Add a new task" type="text" id="addTodo" @focus="searchBoxClicked"/>
+        <button class="plusButton" @click="addTodo">+</button>
+        <div class="modal-fade" id="addTaskPopup" tabindex="-1" aria-labelledby="addTaskPopupLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content" v-html="popupTemplate"></div>
+          </div>
+        </div>
       </div>
       <div class="todoListBox">
         <ul>
@@ -24,17 +33,20 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { Modal } from 'bootstrap';
 
 export default {
   setup() {
     const todos = ref([]);
     const newTodo = ref('');
+    const newTodoDescription = ref('');
+    const isPopupVisible = ref(false);
 
     const addTodo = () => {
-      if (newTodo.value.trim()) { // Check for empty input
+      if (newTodo.value.trim()) {
         todos.value.push({
-          id: Math.random().toString(36).slice(2, 7), // Generate unique ID
+          id: Math.random().toString(36).slice(2, 7),
           text: newTodo.value,
           completed: false,
         });
@@ -42,12 +54,92 @@ export default {
       }
     };
 
+    const searchBoxClicked = () => {
+      const modal = new Modal(document.getElementById('addTaskPopup'));
+      isPopupVisible.value = true;
+      modal.show();
+    };
+
+    const popupTemplate = `
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add New Task</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <input v-model="newTodo" type="text" placeholder="Task Name">
+          <input v-model="newTodoDescription" type="text" placeholder="Task Description">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" @click="addTodo">Add</button>
+        </div>
+      </div>
+    `;
+
+    onMounted(() => {
+      const inputBox = document.getElementById("addTodo");
+      if (inputBox) {
+        inputBox.addEventListener('focus', searchBoxClicked);
+      }
+    });
+
     const now = new Date();
     const datetime = now.toLocaleString();
     const datetimeArray = datetime.split("/", 2);
+    let dateName = "";
 
-    return { todos, newTodo, addTodo, datetimeArray }; // Include datetime in setup
+    switch (datetimeArray[1]) {
+      case "01":
+        dateName = "Jan";
+        break;
+      case "02":
+        dateName = "Feb";
+        break;
+      case "03":
+        dateName = "Mar";
+        break;
+      case "04":
+        dateName = "Apr";
+        break;
+      case "05":
+        dateName = "May";
+        break;
+      case "06":
+        dateName = "Jun";
+        break;
+      case "07":
+        dateName = "Jul";
+        break;
+      case "08":
+        dateName = "Aug";
+        break;
+      case "09":
+        dateName = "Sep";
+        break;
+      case "10":
+        dateName = "Oct";
+        break;
+      case "11":
+        dateName = "Nov";
+        break;
+      case "12":
+        dateName = "Dec";
+        break;
+    }
+
+    return {
+      todos,
+      newTodo,
+      newTodoDescription,
+      addTodo,
+      searchBoxClicked,
+      datetimeArray,
+      dateName,
+      isPopupVisible,
+      popupTemplate,
+    };
   }
 };
-</script>
 
+</script>
